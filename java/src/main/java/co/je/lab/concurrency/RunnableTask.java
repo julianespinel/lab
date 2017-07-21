6d8de34c0bb250d4e5d74c013c28776d35888423
Julian Espinel <julianespinel@gmail.com>
2017-07-21 08:47:55 -0500

    Add concurrency java experiments.

---
+++ java/src/main/java/co/je/lab/concurrency/RunnableTask.java
@@ -0,0 +1,11 @@
+package co.je.lab.concurrency;
+
+/**
+ * Created by julian on 20/07/17.
+ */
+public class RunnableTask implements Runnable {
+
+    public void run() {
+        System.out.println("********************** 1");
+    }
+}
