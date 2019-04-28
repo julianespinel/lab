63234019666c335a7a806f88250346ddecf14f3f
Julian Espinel <julianespinel@gmail.com>
2019-04-28 17:49:24 +0300

    Add method to get a Message from a JsonNode.

--- kafka/src/main/java/com/julianespinel/entities/Message.java
+++ kafka/src/main/java/com/julianespinel/entities/Message.java
@@ -2,6 +2,9 @@ package com.julianespinel.entities;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class Message {
 
@@ -12,6 +15,13 @@ public class Message {
     public Message(@JsonProperty("id") int id, @JsonProperty("text") String text) {
         this.id = id;
         this.text = text;
+    }
+
+    public static Message from(JsonNode jsonNode) {
+        return new Message(
+            jsonNode.get("id").asInt(),
+            jsonNode.get("text").asText()
+        );
     }
 
     public int getId() {
