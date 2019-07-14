9197125a2d75a4bf48907ae7b7c8178d1d88490a
Julian Espinel <julianespinel@gmail.com>
2019-07-14 19:20:07 +0300

    Get database configuration from file.

--- haskellab/app/Main.hs
+++ haskellab/app/Main.hs
@@ -1,6 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Web.Scotty
 import Messages.Resource
+import Database.PostgreSQL.Simple
+import qualified Data.Yaml.Config as YC
+
+getPsqlConnection dbConfig = do
+  host <- YC.lookup "host" dbConfig
+  port <- YC.lookup "port" dbConfig
+  username <- YC.lookup "username" dbConfig
+  password <- YC.lookup "password" dbConfig
+  database <- YC.lookup "database" dbConfig
+  connect defaultConnectInfo
+    { connectHost = host
+    , connectPort = port
+    , connectUser = username
+    , connectPassword = password
+    , connectDatabase = database
+    }
 
 main :: IO ()
-main = someFunc
+main = do
+  config <- YC.load "./dev.yml"
+  dbConfig <- YC.subconfig "database" config
+  psqlConn <- getPsqlConnection dbConfig
+
