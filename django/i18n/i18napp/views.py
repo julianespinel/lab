6f40ab69601e8659909993b2ecc7ac277a62f5d7
Julian Espinel <julianespinel@gmail.com>
2025-01-08 13:37:23 -0500

    feat: understand how i18n and i10n work in Django (#23)
    ## Main changes
    
    1. feat: working Django hello world
    2. feat: add i18napp with index view
    3. feat: support translations of dynamic content
    4. feat: support translations in static content

---
+++ django/i18n/i18napp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.utils import timezone
+from django.utils.translation import gettext as _
+
+
+def index_view(request):
+    context = {
+        'greeting': _("Welcome to our Localization Project!"),
+        'large_number': 12345.67,
+        'current_date': timezone.now(),
+        'redirect_to': request.path,
+        'current_locale': request.LANGUAGE_CODE,
+    }
+    return render(request, 'index.html', context)
