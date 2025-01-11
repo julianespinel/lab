dc4093cba6178345de0dd49a9f72257fa433102d
Julian Espinel <julianespinel@gmail.com>
2025-01-11 14:27:40 -0500

    feat: replace offline template

--- django/pwa/pwaapp/urls.py
+++ django/pwa/pwaapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index_view
 
+from .views import index_view, offline_view
 
 urlpatterns = [
     path('', index_view, name='index'),
+    path('offline', offline_view, name='offline'),
 ]
--- django/pwa/pwaapp/views.py
+++ django/pwa/pwaapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+
 def index_view(request):
     context = {}
     return render(request, 'index.html', context)
+
+
+def offline_view(request):
+    context = {}
+    return render(request, 'offline2.html', context)
