7b2c3dd0a06bc1344374a9a6562a771c1eea4992
Julian Espinel <julianespinel@gmail.com>
2025-01-22 13:08:22 -0500

    feat: add two other views to test offline functionality

--- django/pwa/pwaapp/views.py
+++ django/pwa/pwaapp/views.py
@@ -6,6 +6,16 @@ def index_view(request):
     return render(request, 'index.html', context)
 
 
+def one_view(request):
+    context = {}
+    return render(request, 'one.html', context)
+
+
+def two_view(request):
+    context = {}
+    return render(request, 'two.html', context)
+
+
 def offline_view(request):
     context = {}
     return render(request, 'offline2.html', context)
