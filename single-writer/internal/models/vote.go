c626610ed41c1afcefabd2469b4d169036c6eca7
Julian Espinel <julianespinel@gmail.com>
2025-01-25 12:14:17 -0500

    style: add missing blank line at the end of the file

--- single-writer/internal/models/vote.go
+++ single-writer/internal/models/vote.go
@@ -3,11 +3,11 @@ package models
 import "time"
 
 type Vote struct {
-    ID        int64     `json:"id"`
-    Option    string    `json:"option"`
-    CreatedAt time.Time `json:"created_at"`
+	ID        int64     `json:"id"`
+	Option    string    `json:"option"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 type VoteRequest struct {
-    Option string `json:"option"`
-} 
+	Option string `json:"option"`
+}
