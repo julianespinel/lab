01ec27bd80c1ee9b7b54d26a909abdefcceeb48d
Julian Espinel <julianespinel@gmail.com>
2025-02-21 17:02:12 -0500

    refactor: move client interface and mock implementation to its own file

---
+++ crypto/usdc-ethereum/internal/clients/interfaces.go
@@ -0,0 +1,15 @@
+package clients
+
+import (
+	"context"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/core/types"
+)
+
+type EthClientInterface interface {
+	BlockNumber(ctx context.Context) (uint64, error)
+	HeaderByNumber(ctx context.Context, number *big.Int) (*types.Header, error)
+	FilterLogs(ctx context.Context, query ethereum.FilterQuery) ([]types.Log, error)
+}
