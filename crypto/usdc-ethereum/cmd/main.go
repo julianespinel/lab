774ee13ae689ad730fb14c5f941d7a8b2fe43dcc
Julian Espinel <julianespinel@gmail.com>
2025-02-20 19:00:03 -0500

    feat: get wallet transactions

--- crypto/usdc-ethereum/cmd/main.go
+++ crypto/usdc-ethereum/cmd/main.go
@@ -4,9 +4,49 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/julianespinel/lab/crypto/usdc-ethereum/config"
 	"github.com/julianespinel/lab/crypto/usdc-ethereum/internal/ethereum"
+	"github.com/julianespinel/lab/crypto/usdc-ethereum/internal/ethereum/usdc"
+	"github.com/julianespinel/lab/crypto/usdc-ethereum/pkg/models"
 )
+
+func fetchAndProcessContractEvents(client *ethclient.Client, cfg *config.Config) ([]models.ContractEvent, error) {
+	fmt.Println("Fetching USDC events between:", cfg.StartDate, "and", cfg.EndDate)
+	events, err := usdc.FetchUSDCContractLogsByDateRange(client, cfg.StartDate, cfg.EndDate)
+	if err != nil {
+		return nil, fmt.Errorf("error fetching USDC events: %v", err)
+	}
+
+	if len(events) == 0 {
+		return nil, fmt.Errorf("no USDC events found")
+	}
+
+	ethereum.DisplayEvents(events)
+	ethereum.CalculateTotalAmount(events)
+	ethereum.CalculateTotalAmountPerAddress(events)
+
+	return events, nil
+}
+
+func fetchAndProcessWalletTransactions(cfg *config.Config, numTransactions int) ([]models.ContractEvent, error) {
+	fmt.Println("Fetching transactions for wallet:", cfg.WalletAddress)
+
+	events, err := usdc.FetchTransactionsFromEtherscan(cfg.EtherscanAPIKey, cfg.WalletAddress, numTransactions)
+	if err != nil {
+		return nil, fmt.Errorf("error fetching wallet events: %v", err)
+	}
+
+	if len(events) == 0 {
+		return nil, fmt.Errorf("no wallet events found")
+	}
+
+	ethereum.DisplayEvents(events)
+	ethereum.CalculateTotalAmount(events)
+	ethereum.CalculateTotalAmountPerAddress(events)
+
+	return events, nil
+}
 
 func main() {
 	// Load configuration
@@ -22,18 +62,13 @@ func main() {
 	}
 	defer client.Close()
 
-	// Fetch USDC events
-	fmt.Println("Fetching USDC events between:", cfg.StartDate, "and", cfg.EndDate)
-	events, err := ethereum.FetchUSDCContractLogsByDateRange(client, cfg.StartDate, cfg.EndDate)
+	_, err = fetchAndProcessContractEvents(client, cfg)
 	if err != nil {
-		log.Fatalf("Error fetching USDC events: %v", err)
+		log.Fatalf("%v", err)
 	}
 
-	if len(events) == 0 {
-		log.Fatalf("No USDC events found")
+	_, err = fetchAndProcessWalletTransactions(cfg, 10)
+	if err != nil {
+		log.Fatalf("%v", err)
 	}
-
-	ethereum.DisplayEvents(events)
-	ethereum.CalculateTotalAmount(events)
-	ethereum.CalculateTotalAmountPerAddress(events)
 }
